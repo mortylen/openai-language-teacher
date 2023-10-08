71d1d74f72999ededfcf32ac144bee7132c4d708
mortylen <123174309+mortylen@users.noreply.github.com>
2023-10-08 07:14:56 +0000

    Update model_conversation

--- src/model_conversation.rs
+++ src/model_conversation.rs
@@ -6,16 +6,17 @@ use crate::openai_executor::send_message;
 use crate::openai_executor::get_user_input;
 
 pub async fn model_conversation(ywt_api_key: &String, mut ai_chat: GPTRequest, target_language: &String, native_language: &String) -> Result<(), Error> {
-	//let system_message: String = format!("I would like you to split each of your replies into three part. \nIn the first part called as 'Correction:', write the correct version of my sentence in {0} language. \nIn second pard called as 'Note', description and explain of where I made grammatical mistakes in my {0} language, this part you write in {1} language. \nIn the third part called as 'Conversation:', feel free to respond to my statement and continue the conversation in {0} language.", &target_language, &native_language);
     let system_message: String = format!("You are my {0} teacher. I would like you to split each of your replies into three part. \nIn the first part called as 'Correction:', write the correct version of my sentence in {0} language. \nIn second pard called as 'Note', description and explain of where I made grammatical mistakes in my {0} language, this part you must described in {1} language. \nIn the third part called as 'Conversation:', feel free to respond to my statement and continue the conversation in {0} language.", &target_language, &native_language);
     println!("\n");
     println!("Please start the conversation.");
-    
+
+    let mut user_message: String;
     loop {
         ai_chat.remove_system_message();
 		ai_chat.add_system_message(&system_message);
         println!("You: "); 
-        ai_chat.add_message(Message{role: "user".to_string(), content: get_user_input()});
+        user_message = format!("Fix my {0}. Correct my mistakes in this paragraph to standard {0} and write the explanation in {1}. \n '{2}'", &target_language, &native_language, get_user_input());
+        ai_chat.add_message(Message{role: "user".to_string(), content: user_message});
         println!("\n");
         
         match send_message(&ai_chat, &ywt_api_key).await {
